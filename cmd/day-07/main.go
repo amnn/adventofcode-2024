73ff8e52647ba11fb1b699e54d55288e24566b64
Ashok Menon <ashok@mystenlabs.com>
2024-12-08 02:28:33 +0000

    day-07: Faster backward iteration approach
    Use the backwards algorithm which uses a recursive algorithm which pops
    terms off the back of the list, checks where they could be applied using
    either `+` or `*`, and then recurses with an adjusted target.
    
    This approach detects bad branches early and cuts them short, avoiding
    wasted work.

--- cmd/day-07/main.go
+++ cmd/day-07/main.go
@@ -54,7 +54,7 @@ func part1(equations []equation) int64 {
 	total := int64(0)
 
 	for _, e := range equations {
-		if isSatisfiable(e) {
+		if isSatisfiable(e.target, e.terms /* withConcat */, false) {
 			total += e.target
 		}
 	}
@@ -64,7 +64,7 @@ func part1(equations []equation) int64 {
 
 func part2(equations []equation) (total int64) {
 	for _, e := range equations {
-		if isSatifisableWithConcat(e) {
+		if isSatisfiable(e.target, e.terms /* withConcat */, true) {
 			total += e.target
 		}
 	}
@@ -74,42 +74,27 @@ func part2(equations []equation) (total int64) {
 
 // Returns true if ther is some combination of operations that will result in
 // `e` satisfying its target.
-func isSatisfiable(e equation) bool {
-	for ops := int64(0); ops < int64(1)<<len(e.terms); ops++ {
-		if evaluate(e, ops) {
-			return true
-		}
+func isSatisfiable(target int64, terms []term, withConcat bool) bool {
+	if len(terms) == 1 {
+		return terms[0].value == target
+	}
+
+	last := terms[len(terms)-1]
+	rest := terms[:len(terms)-1]
+
+	if target%last.value == 0 && isSatisfiable(target/last.value, rest, withConcat) {
+		return true
+	}
+
+	if target > last.value && isSatisfiable(target-last.value, rest, withConcat) {
+		return true
+	}
+
+	if withConcat && (target-last.value)%last.pad10 == 0 && isSatisfiable(target/last.pad10, rest, withConcat) {
+		return true
 	}
 
 	return false
-}
-
-// Evaluate an equation with the operations specified in `ops`. Operations can
-// be `+` or `*`, evaluated from left to right, and they are represented by the
-// bits of an integer. The least significant bit represents the first
-// operation, and so on. A bit set to `0` means that the operation is `+`, and
-// a bit set to `1` means that the operation is `*`.
-//
-// Returns `true` if the equation evaluates to the target value, `false`
-// otherwise.
-func evaluate(e equation, ops int64) bool {
-	actual := e.terms[0].value
-
-	for i, term := range e.terms[1:] {
-		// If the actual value exceeds the target, we can stop early, because it's
-		// never going to get smaller
-		if actual > e.target {
-			return false
-		}
-
-		if ops&(1<<i) == 0 {
-			actual += term.value
-		} else {
-			actual *= term.value
-		}
-	}
-
-	return actual == e.target
 }
 
 // Logarithmic time integer exponentiation
@@ -126,45 +111,4 @@ func Pow(x, y int64) int64 {
 	}
 
 	return result
-}
-
-// Like `isSatisfiable` but with an additional concatenation operation.
-func isSatifisableWithConcat(e equation) bool {
-	for ops := int64(0); ops < Pow(int64(3), int64(len(e.terms))); ops++ {
-		if evaluateWithConcat(e, ops) {
-			return true
-		}
-	}
-
-	return false
-}
-
-// Like `evaluate`, but `ops` is a stream of one of three operations: addition,
-// multiplication, and concatenation. It is still represented by a number, but
-// it is decoded by converting into base 3.
-func evaluateWithConcat(e equation, ops int64) bool {
-	actual := e.terms[0].value
-
-	for _, term := range e.terms[1:] {
-		// If the actual value exceeds the target, we can stop early, because it's
-		// never going to get smaller
-		if actual > e.target {
-			return false
-		}
-
-		op := ops % 3
-		ops /= 3
-
-		switch op {
-		case 0:
-			actual += term.value
-		case 1:
-			actual *= term.value
-		case 2:
-			actual *= term.pad10
-			actual += term.value
-		}
-	}
-
-	return actual == e.target
 }
