d723d2765f8ee09b6732c49635f1ee54e3c1b062
Ashok Menon <ashok@mystenlabs.com>
2024-12-14 16:17:34 +0000

    day-13: part 2

--- cmd/day-13/main.go
+++ cmd/day-13/main.go
@@ -10,9 +10,22 @@ type machine struct {
 	ax, ay, bx, by, x, y int
 }
 
+const (
+	A_COST = 3
+	B_COST = 1
+)
+
 func main() {
 	input := readInput(os.Stdin)
-	fmt.Println("Part 1:", part1(input))
+
+	input1 := make([]machine, len(input))
+	copy(input1, input)
+
+	input2 := make([]machine, len(input))
+	copy(input2, input)
+
+	fmt.Println("Part 1:", part1(input1))
+	fmt.Println("Part 2:", part2(input2))
 }
 
 func part1(input []machine) (total int) {
@@ -24,32 +37,33 @@ func part1(input []machine) (total int) {
 	return
 }
 
-// Brute force solver which tries all possible combinations of button presses
-func solve(m machine) (min int, soluble bool) {
-	const (
-		kACost      = 3
-		kBCost      = 1
-		kMaxPresses = 100
-		kMaxCost    = kACost*kMaxPresses + kBCost*kMaxPresses
-	)
-
-	min = kMaxCost
-	for as := 0; as <= kMaxPresses; as++ {
-		for bs := 0; bs <= kMaxPresses; bs++ {
-			if m.ax*as+m.bx*bs == m.x && m.ay*as+m.by*bs == m.y {
-				cost := kACost*as + kBCost*bs
-				if cost < min {
-					min = cost
-				}
-			}
+func part2(input []machine) (total int) {
+	for _, m := range input {
+		m.x += 10000000000000
+		m.y += 10000000000000
+		if cost, ok := solve(m); ok {
+			total += cost
 		}
 	}
+	return
+}
 
-	if kMaxCost == min {
-		return 0, false
-	} else {
-		return min, true
+func solve(m machine) (cost int, soluble bool) {
+	det := m.ax*m.by - m.ay*m.bx
+	if det == 0 {
+		return
 	}
+
+	a := m.by*m.x - m.bx*m.y
+	b := m.ax*m.y - m.ay*m.x
+
+	if a%det != 0 || b%det != 0 {
+		return
+	}
+
+	soluble = true
+	cost = A_COST*(a/det) + B_COST*(b/det)
+	return
 }
 
 func readInput(r io.Reader) []machine {
@@ -70,7 +84,6 @@ func readInput(r io.Reader) []machine {
 		}
 
 		input = append(input, buf)
-
 		if _, err := fmt.Fscanf(r, "\n"); err != nil {
 			break
 		}
